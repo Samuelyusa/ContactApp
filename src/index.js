9d616993ca9483f8a07251bbdffb687163ed8b10
Samuelyusa <samuelyusa@gmail.com>
2022-11-24 17:27:02 +0700

    First Commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import ContactApp from './components/ContactApp';
+
+// styling
+import './styles/style.css';
+
+const root = createRoot(document.getElementById('root'));
+root.render(
+    <BrowserRouter>
+        <ContactApp />
+    </BrowserRouter>
+);
